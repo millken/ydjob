d3ec5e40463acbb7d37bef59c683a88215c6adb4
millken <millken@gmail.com>
2013-03-27 23:05:02 +0800

    added queue

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"time"
+	"Config"
+	"Job"
 )
 
 func ready1() {
 	for {
-	time.Sleep(time.Minute * 1)
+	time.Sleep(time.Second * Config.GetLoopTime().Mail)
 	fmt.Printf("unix time:")
 	}
 }
@@ -17,9 +19,11 @@ func ready2() {
 	fmt.Printf("%d\n", time.Now().Unix())
 	}
 }
+
 func main() {
-	go ready1()
+	go (&Job.Jobmail{}).Run()
 	go ready2()
-    fmt.Printf("hello, world\n")
+    fmt.Printf("hello, world %s\n", Config.GetBeanstalk().MailQueue)
+    fmt.Printf("%d", Config.GetLoopTime().Mail)
 	time.Sleep(2e8 * time.Second)
 }
---
+++ src/Config/Config.go
@@ -0,0 +1,95 @@
+package Config
+
+import (
+	"Utils"
+	"encoding/xml"
+	"os"
+	"flag"
+	"time"
+)
+
+
+type redisConfig struct {
+	Addr, Port string
+	PoolSize   int
+}
+type loopTime struct {
+	Mail, Mobile, Crawlkangle time.Duration
+}
+
+type smtpConfig struct {
+	Host, Username, Password, From string
+}
+
+type beanstalkConfig struct {
+	Server, MailQueue, MobileQueue string
+}
+
+type VsConfig struct {
+	XMLName    xml.Name `xml:"Config"`
+	Redis      redisConfig
+	Smtp       smtpConfig
+	TickTime time.Duration
+	LoopTime  loopTime
+	Beanstalk   beanstalkConfig	
+}
+
+var vsConfig VsConfig
+var ConfigFile string
+var DataFile string
+func init() {
+	flag.StringVar(&ConfigFile,"c","config.xml","config file path")
+	flag.Parse()
+	if ConfigFile==""{
+		ConfigFile="./config.xml"
+	}
+	ParseXml(ConfigFile)
+}
+/**
+解析xml文件
+**/
+func ParseXml(configFile string){
+	file, err := os.Open(configFile)
+	if err != nil {
+		Utils.LogPanicErr(err)
+		return
+	}
+	xmlObj := xml.NewDecoder(file)
+	err = xmlObj.Decode(&vsConfig)
+	if err != nil {
+		Utils.LogPanicErr(err)
+		return
+	}
+	Utils.LogInfo("parse xml=%v\n",vsConfig)
+}
+/**
+得到redis的配置
+**/
+func GetRedisConfig() redisConfig {
+	return vsConfig.Redis
+}
+
+/**
+ * get loop time
+ */
+func GetLoopTime() loopTime {
+	return vsConfig.LoopTime
+}
+
+/*
+ * smtp config
+ */
+func GetSmtp() smtpConfig {
+	return vsConfig.Smtp
+}
+
+/*
+ * beantalk queue
+ */
+func  GetBeanstalk() beanstalkConfig {
+	return vsConfig.Beanstalk
+}
+
+func GetTickTime()time.Duration{
+	return vsConfig.TickTime
+}
